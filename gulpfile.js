67eb1eb40f2f6a429c71fb1f2a47e25a358f6d33
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-08-09 09:19:53 +0100

    section header done

--- gulpfile.js
+++ gulpfile.js
@@ -43,24 +43,24 @@ let style = () => {
         // 8. stream changes to all browsers
         .pipe(browserSync.stream());
 }
-// let stylePublic = () => {
-//     // 1. where is my scss file
-//     return gulp.src('src/sass/**/*.scss')
-//         // 2. pass the file through sass compiler
-//         .pipe(sass().on('error', sass.logError))
-//         // 3. pass the file through autoprifixer
-//         .pipe(autoprefixer())
-//         // 4. pass the file through css minifier
-//         .pipe(cssnano())
-//         // 5. intilizing sourcemaps
-//         .pipe(sourcemaps.init())
-//         // 6. pass the files through sourcemaps ?
-//         .pipe(sourcemaps.write('.'))
-//         // 7. where do i save the compiled CSS ?
-//         .pipe(gulp.dest('public/assets/css'))
-//         // 8. stream changes to all browsers
-//         .pipe(browserSync.stream());
-// }
+let stylePublic = () => {
+    // 1. where is my scss file
+    return gulp.src('src/sass/**/*.scss')
+        // 2. pass the file through sass compiler
+        .pipe(sass().on('error', sass.logError))
+        // 3. pass the file through autoprifixer
+        .pipe(autoprefixer())
+        // 4. pass the file through css minifier
+        .pipe(cssnano())
+        // 5. intilizing sourcemaps
+        .pipe(sourcemaps.init())
+        // 6. pass the files through sourcemaps ?
+        .pipe(sourcemaps.write('.'))
+        // 7. where do i save the compiled CSS ?
+        .pipe(gulp.dest('public/assets/css'))
+        // 8. stream changes to all browsers
+        .pipe(browserSync.stream());
+}
 
 let userref = () => {
     return gulp.src('./src/*.html')
@@ -76,24 +76,24 @@ let userref = () => {
         .pipe(gulp.dest('dist'));
 }
  
-// let jspublic = () => {
-//     return gulp.src('./src/includes/build_jspublic.html')
-//         .pipe(useref())
-//         .pipe(gulpIf('*.js', terser()))
-//         // 3. pass the file through css minifier
-//         // .pipe(gulpIf('*.css', cssnano()))
+let jspublic = () => {
+    return gulp.src('./src/includes/build_jspublic.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', terser()))
+        // 3. pass the file through css minifier
+        // .pipe(gulpIf('*.css', cssnano()))
 
-//         .pipe(gulp.dest('public/assets'));
-// }
+        .pipe(gulp.dest('public/assets'));
+}
 
-// let fileincludes = () => {
-//     return gulp.src('./*.html')
-//     .pipe(fileinclude({
-//         prefix: '@@',
-//         basepath: '@file'
-//     }))
-//     .pipe(gulp.dest('dist'));
-// }
+let fileincludes = () => {
+    return gulp.src('./*.html')
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+    }))
+    .pipe(gulp.dest('dist'));
+}
 
 
 let imgminify = () => {
@@ -116,7 +116,7 @@ function watch() {
     gulp.watch('./src/sass/**/*.scss', userref);
     gulp.watch('./src/*.html', userref);
     gulp.watch('./src/js/**/*.js', userref);
-    gulp.watch('./src/js/**/*.js', jspublic);
+    // gulp.watch('./src/js/**/*.js', jspublic);
     gulp.watch('./src/includes/*.html', userref);
     gulp.watch('./**/*.html').on('change', browserSync.reload);
     gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
