7b22f512e62adcffab70aded840563ca92f9461c
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-08-12 22:22:54 +0100

    centered buy now

--- index.php
+++ index.php
@@ -374,7 +374,7 @@
   <p class="footer__designer">DESIGNED BY <a href="https://github.com/Med-MG"><span>MED MOUIGUINA</span></a></p>
 </section>
 <div class="buy-now-sticky">
-  <a href="#buy-form" class="btn btn--gold button-sticky b-center">شراء الآن</a>
+  <a href="#buy-form" class="btn btn--gold button-sticky b-center" style="margin: 0 auto;">شراء الآن</a>
   <a href="https://wa.me/212620226054?text=%D8%A3%D8%B1%D9%8A%D8%AF%20%D8%B4%D8%B1%D8%A7%D8%A1%20%D9%85%D8%A8%D8%B1%D8%AF%20%D8%A7%D9%84%D8%AC%D9%88%20%D8%A7%D9%84%D9%85%D8%AD%D9%85%D9%88%D9%84" class="button-whatsapp button-sticky  hidden"><img src="dist/images/icons8-whatsapp-64.png" class="button-whatsapp__img" /> الواتساب</a>
 </div>
 <div class="<?php
@@ -398,8 +398,8 @@ if (isset($_GET['order'])) {
 				</div>
 				<h3 class="heading-tertiary">شكرا جزيلاً على ثقتكم</h3>
 				<p class="popup__text">
-					لقد تم تسجيل طلبكم سنتصل بك هاتفيا من أجل تأكيده
-					سنقوم بإرسال طلبكم إلى العنوان الذي قمت بتعبئتة <br>
+					لقد تم تسجيل الطلب !  سيتصل بكم فريق الدعم الفني
+          لتأكيد طلبكم في أقرب وقت ممكن <br>
 				</p>
 				<a class="btn btn--gold button-popup" href="./">استمر في التسوق</a>
 			</div>
